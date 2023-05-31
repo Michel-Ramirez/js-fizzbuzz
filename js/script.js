f16076550883a55706b5e952ba86651542a4f8fa
Michel-Ramirez <michel.ramirez19@gmail.com>
2023-05-31 09:44:05 +0200

    fix operation in JS

--- js/script.js
+++ js/script.js
@@ -4,26 +4,33 @@ console.log('JS OK')
 const number = document.getElementById('number')
 
 // Creo Variabili di appoggio
-
-let stampa = ''
+let stamp = ''
+let box = ''
 
 // ciclo che genera i numeri da 1 a 100
 for (let i = 1; i <= 100; i++){
    
     // Se è un multiplo di 3 stampa fizz
-    if (i % 3 === 0) {
+    if (i % 3 === 0 && i % 5 === 0 ){
+
+        stamp = 'FizzBuzz'
+
+    } else if (i % 3 === 0) {
         
-        stampa = 'Fizz'
+        stamp = 'Fizz'
         
         // Se è un multiplo di 5 stampa fizz
     } else if (i % 5 === 0)
     
-        stampa = 'Buzz'
+        stamp = 'Buzz'
     // altrimenti stampa il resto
     else {
-        stampa = i
+        stamp = i
     }
 
-    console.log(stampa)
+    console.log(stamp)
 }
 
+box = `<div>${stamp}</div>`;
+
+number.innerHTML = box;
